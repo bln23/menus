7db438a67dd9ba2fb6fd34f026b386c86e043a12
Belén <be23mg@gmail.com>
2018-04-15 11:21:17 +0200

    remove list to do

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Category from './Category.js';
-import ToDoItem from './ToDoItem.js';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       expanded: '',
-      listToDo: null,
-      item: []
     }
     this.handleExpanded = this.handleExpanded.bind(this);
   }
@@ -18,26 +15,6 @@ class App extends Component {
     this.setState({
       expanded: pExpanded
     })
-  }
-
-  addText = event => {
-    this.setState({
-      list: event.target.value
-
-    })
-  }
-
-  sendToDo = () => {
-    this.setState( prevState => { return {
-      item: [...prevState.item, prevState.list],
-      list: ''
-    }})
-  }
-
-  remove = index => {
-    const item = [...this.state.item]
-    item.splice(index, 1)
-    this.setState({ item: item})
   }
 
   render() {
@@ -60,15 +37,6 @@ class App extends Component {
               icon="angle-down"
               visible={this.state.expanded}
               handleExpanded={this.handleExpanded}/>
-        </ul>
-        <div className="Form">
-          <input value={this.state.list} onChange={this.addText} type="text"/>
-          <button className="button-add" onClick={this.sendToDo}>Add</button>
-        </div>
-        <ul className="list">
-          {this.state.item.map((itemList, index) => <ToDoItem key={index} itemList={itemList}
-            remove={()=>{this.remove(index)}}/>
-          )}
         </ul>
       </div>
     )
